20e931ecb2c9ef71ef04940f4a93120be45e666d
Lescenco Andrei Bogdan <lescee@gmail.com>
2011-06-24 18:40:42 +0300

    added autogenerate map resources

--- scripts/draw.js
+++ scripts/draw.js
@@ -4,27 +4,45 @@ function drawww(){
 	//context.strokeRect(50,25,150,100);	
 	var pic = new Image();
 	pic.src = "images/hexagonrgb.png";
-	context.drawImage(pic,0,90,100,100);//coloana1
-	context.drawImage(pic,0,180,100,100);
-	context.drawImage(pic,0,270,100,100);
-	context.drawImage(pic,78,45,100,100);//coloana2
-	context.drawImage(pic,78,135,100,100);
-	context.drawImage(pic,78,225,100,100);
-	context.drawImage(pic,78,315,100,100);
-	context.drawImage(pic,156,0,100,100);//coloana3
-	context.drawImage(pic,156,90,100,100);
-	context.drawImage(pic,156,180,100,100);
-	context.drawImage(pic,156,270,100,100);
-	context.drawImage(pic,156,360,100,100);
-	context.drawImage(pic,234,45,100,100);//coloana4
-	context.drawImage(pic,234,135,100,100);
-	context.drawImage(pic,234,225,100,100);
-	context.drawImage(pic,234,315,100,100);
-	context.drawImage(pic,312,90,100,100);//coloana5
-	context.drawImage(pic,312,180,100,100);
-	context.drawImage(pic,312,270,100,100);
+	context.drawImage(getPic(),0,90,100,100);//coloana1
+	context.drawImage(getPic(),0,180,100,100);
+	context.drawImage(getPic(),0,270,100,100);
+	context.drawImage(getPic(),78,45,100,100);//coloana2
+	context.drawImage(getPic(),78,135,100,100);
+	context.drawImage(getPic(),78,225,100,100);
+	context.drawImage(getPic(),78,315,100,100);
+	context.drawImage(getPic(),156,0,100,100);//coloana3
+	context.drawImage(getPic(),156,90,100,100);
+	context.drawImage(getPic(),156,180,100,100);
+	context.drawImage(getPic(),156,270,100,100);
+	context.drawImage(getPic(),156,360,100,100);
+	context.drawImage(getPic(),234,45,100,100);//coloana4
+	context.drawImage(getPic(),234,135,100,100);
+	context.drawImage(getPic(),234,225,100,100);
+	context.drawImage(getPic(),234,315,100,100);
+	context.drawImage(getPic(),312,90,100,100);//coloana5
+	context.drawImage(getPic(),312,180,100,100);
+	context.drawImage(getPic(),312,270,100,100);
 }
-function reset(){
-	var cnvs = document.getElementById("hexagon");
-	cnvs.width = cnvs.width;
+var indexOre = 0;//0
+var indexBrick = 0;//1
+var indexLumber = 0;//2
+var indexWool = 0;//3
+var indexGrain = 0;//4
+var list = new Array(1,0,2,1,4,3,2,1,3,0,0,1,3,2,4,4,3,2,5);
+
+function getPic(){
+	testPic = new Image();
+	val = Math.floor(Math.random()*list.length);
+	console.log(val);
+	testPic.src="images/"+list[val]+".png";
+	list.splice(val,1);
+	return testPic;
+}
+function getRandExcept(x){
+	val = Math.floor(Math.random()*5);
+	if(x===val)
+		return getRandExcept(x);
+	else 
+		return val;
 }
